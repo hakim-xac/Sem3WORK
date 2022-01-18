da4dd528f14cea6def7e58fefbce487ab2760e7f
Андрей Хакимов <hakim.xac@gmail.com>
2022-01-18 20:04:33 +0700

    refactoring

--- Sem3WORK/Interface.h
+++ Sem3WORK/Interface.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <queue>
 #include <iostream>
+#include <type_traits>
 #include "enums.h"
 
 namespace WORK {
@@ -32,45 +33,37 @@ namespace WORK {
 
 
 		TypeArray myTypeArray;
-
-		void addToStatusBar(const std::string& str, bool isFormated = true);			// добавляет в очередь статус бара, строку для вывода
-		void addToStatusBar(const std::string&& str, bool isFormated = true);
+			
+		void addToStatusBar(const std::string&& str, bool isFormated = true);			// добавляет в очередь статус бара, строку для вывода
 
 		bool flagClearArrayAndHash								{ true };				// флаг пустого массива и, следовательно, хеш-таблиц
 
 
 	public:
 		
-		Interface(TypeArray& myTypeArray);
-
 		Interface(TypeArray&& myTypeArray);
 
 		constexpr size_t getMaxTableWidth()			const;					// возврат ширины максимальной длины используемой строки в приложении
 		constexpr size_t getMaxTableColumns()		const;					// возврат количества колонок "таблицы", при выводе данных массива и хеш-таблиц
-		constexpr bool getFlagClearArrayAndHash()	const;					// флаг пустого массива и хеш-таблиц
+
 		Keys getActiveKey()							const;					// возврат текущего кода клавиши
 		void setActiveKey(Keys key);										// задает код клавиши
 
-		void setFlagClearArray(bool flag);									// задает флаг, сигнализирующий очистку
-
 		constexpr void showHeader();										// выводит заголовок
 		constexpr void showMenu();											// выводит меню
-		void showStatusBar();												// выводит информацию из статцс бара
-		void showStatusType();												// выводит информацию о массиве и хеш-таблице
+		void showStatusBar();												// выводит информацию из статус бара
 
-		constexpr  std::string delimiter(char del = '=') const;				// выводит разделитель
+		constexpr  std::string delimiter(char del = '=') const;				// возврат разделителя
 
 
 		///
 		/// генерирует строку согласно формату программы с 1 параметром
 		///
-		constexpr const std::string generatingStrings(const std::string& str, char del = ' ') const;
 		constexpr const std::string generatingStrings(const std::string&& str, char del = ' ') const;
 
 		///
 		/// генерирует строку согласно формату программы с 2 параметрами
 		///
-		constexpr const std::string generatingStrings(const std::string& str, const std::string& str2, char del = ' ') const;
 		constexpr const std::string generatingStrings(const std::string&& str, const std::string&& str2, char del = ' ') const;
 
 	};
@@ -82,15 +75,6 @@ namespace WORK {
 /***************************************************************************************************************************/
 /***************************************************************************************************************************/
 												/* Определение методов */
-
-
-template <class TypeArray>
-WORK::Interface<TypeArray>
-::Interface(
-	TypeArray& myTypeArray)
-	: myTypeArray(myTypeArray) {
-	generateMenu();
-}
 
 
 template <class TypeArray>
@@ -108,25 +92,18 @@ void WORK::Interface<TypeArray>
 	bufferForMenu.emplace_back(delimiter(' '));
 	bufferForMenu.emplace_back(generatingStrings("Нажмите на клавишу и нажмите ВВОД"));
 	bufferForMenu.emplace_back(delimiter('_'));
-
 	bufferForMenu.emplace_back(delimiter(' '));
-	bufferForMenu.emplace_back(generatingStrings("Вывод"));
-	bufferForMenu.emplace_back(generatingStrings("( 1 )", "Массива", '.'));
-	bufferForMenu.emplace_back(generatingStrings("( 2 )", "Хеш-таблицы (метод прямого связывания)", '.'));
-	bufferForMenu.emplace_back(generatingStrings("( 3 )", "хеш-таблицы (метод открытой адрессации, использованы линейные пробы)", '.'));
-	bufferForMenu.emplace_back(generatingStrings("( 4 )", "хеш-таблицы (метод открытой адрессации, использованы квадратичные пробы)", '.'));
-
-	bufferForMenu.emplace_back(delimiter(' '));
-	bufferForMenu.emplace_back(generatingStrings("Поиск в хеш-таблице"));
-
+	bufferForMenu.emplace_back(generatingStrings("Ответы на решение заданий"));
+	bufferForMenu.emplace_back(delimiter('_'));
+	bufferForMenu.emplace_back(generatingStrings("( 1 )", "Выполнить вручную сортировку методом прямого выбора", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 2 )", "Выполнить вручную шейкерную сортировку", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 3 )", "Выполнить вручную сортировку методом Шелла", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 4 )", "Построить пирамиду", '.'));
 	bufferForMenu.emplace_back(generatingStrings("( 5 )", "метод прямого связывания", '.'));
-	bufferForMenu.emplace_back(generatingStrings("( 6 )", "метод открытой адрессации, использованы линейные пробы", '.'));
-	bufferForMenu.emplace_back(generatingStrings("( 7 )", "метод открытой адрессации, использованы квадратичные пробы", '.'));
-
-	bufferForMenu.emplace_back(delimiter(' '));
-	bufferForMenu.emplace_back(generatingStrings("Прочее"));
-	bufferForMenu.emplace_back(generatingStrings("( 8 )", "Обновить массив и хеш-таблицы новыми данными", '.'));
-	bufferForMenu.emplace_back(generatingStrings("( 9 )", "Задать новый размер массива и хеш-таблиц", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 6 )", "Слияние двух списков", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 7 )", "Выполнить вручную сортировку методом цифровой сортировки", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 8 )", "Выполнить вручную быстрый поиск Буквы А", '.'));
+	bufferForMenu.emplace_back(generatingStrings("( 9 )", "Выполнить вручную быстрый поиск Буквы Я", '.'));
 	bufferForMenu.emplace_back(generatingStrings("( 0 )", "Выход", '.'));
 	bufferForMenu.emplace_back(delimiter());
 }
@@ -160,14 +137,6 @@ constexpr size_t WORK::Interface<TypeArray>
 
 
 template <class TypeArray>
-constexpr bool WORK::Interface<TypeArray>
-::getFlagClearArrayAndHash() const 
-{
-	return flagClearArrayAndHash;
-}
-
-
-template <class TypeArray>
 WORK::Keys WORK::Interface<TypeArray>
 ::getActiveKey() const
 {
@@ -185,34 +154,20 @@ void WORK::Interface<TypeArray>
 
 template <class TypeArray>
 void WORK::Interface<TypeArray>
-::setFlagClearArray(bool flag)
-{
-	flagClearArrayAndHash = flag;
-}
-
-
-template <class TypeArray>
-void WORK::Interface<TypeArray>
-::addToStatusBar(const std::string& str, bool isFormated)
-{
-	return addToStatusBar(std::move(str), isFormated);
-}
-
-
-template <class TypeArray>
-void WORK::Interface<TypeArray>
 ::addToStatusBar(const std::string&& str, bool isFormated)
 {	
 	if (!isFormated) {
-		bufferForStatusBar.emplace(str);
+		bufferForStatusBar.emplace(std::move(str));
 		return;
 	}
+	std::string hr{ delimiter() };
+	std::string hr2{ delimiter(' ')};
 
-	bufferForStatusBar.emplace(delimiter());
-	bufferForStatusBar.emplace(delimiter(' '));
-	bufferForStatusBar.emplace(generatingStrings(str));
-	bufferForStatusBar.emplace(delimiter(' '));
-	bufferForStatusBar.emplace(delimiter());
+	bufferForStatusBar.emplace(hr);
+	bufferForStatusBar.emplace(hr2);
+	bufferForStatusBar.emplace(generatingStrings(std::move(str)));
+	bufferForStatusBar.emplace(hr2);
+	bufferForStatusBar.emplace(hr);
 }
 
 
@@ -220,16 +175,12 @@ template <class TypeArray>
 constexpr void WORK::Interface<TypeArray>
 ::showHeader()
 {
-
-	std::string header{ generatingStrings("Лабораторная работа № 3", "Изучение возможности хэширования данных для организации поиска. ") };
-	std::string header2{ generatingStrings("Группа ПБ-11", "Хакимов А.C.") };
-
 	std::string hr{ delimiter() };
 
 	out << hr;
-	out << header;
+	out << generatingStrings("Контрольная работа");
 	out << delimiter('-');
-	out << header2;
+	out << generatingStrings("Группа ПБ-11", "Хакимов А.C.");
 	out << hr;
 }
 
@@ -252,83 +203,71 @@ void WORK::Interface<TypeArray>
 }
 
 
-
-template <class TypeArray>
-void WORK::Interface<TypeArray>
-::showStatusType()
-{
-	out << generatingStrings("Статус Массива и Хеш-таблицы:", getFlagClearArrayAndHash() ? "ПУСТО" : "ЗАПОЛНЕНЫ");
-	out << delimiter('-');
-	out << generatingStrings("количество элементов массива:", std::to_string(myTypeArray.getSize()));
-	out << delimiter('-');
-	//out << generatingStrings("количество элементов хеш-таблицы:", std::to_string(myTypeHashTableDirectBinding.getSize()));
-	out << delimiter();
-}
-
-
 template <class TypeArray>
 constexpr std::string WORK::Interface<TypeArray>
 ::delimiter(char del) const
 {
-
 	std::string result(maxTableWidth, del);
-	result.at(0) = '#';	result[result.size() - 2] = '#'; result.back() = '\n';
+	result.at(0) = '#';
+	result[result.size() - 2] = '#';
+	result.back() = '\n';
 	return result;
 }
 
 
 template <class TypeArray>
 constexpr const std::string WORK::Interface<TypeArray>
-::generatingStrings(const std::string& str, char del) const
+::generatingStrings(const std::string&& inStr, char del) const
 {
-	return generatingStrings(std::move(str), del);
-}
+	if (inStr.empty()) return {};
 
+	std::string str;
+	if constexpr (std::is_lvalue_reference_v<std::string>) str = inStr;
+	else str = std::move(inStr);
 
-template <class TypeArray>
-constexpr const std::string WORK::Interface<TypeArray>
-::generatingStrings(const std::string&& str, char del) const
-{
-	try {
-		if (str.empty()) throw std::exception("Dont empty string! -> LAB2::MyList::generatingStrings(const std::string& str)");
+	try {		
 		int parity{ str.length() % 2 == 0 };
 		size_t middleSize{ maxTableWidth > (str.length() + 2) ? (maxTableWidth - str.length() - 2) / 2 : 0 };
 
 		std::string middle(middleSize - parity, del);
-		std::string result{ "#" + middle + str + (parity ? " " : "") + middle + "#\n" };
 
-		return result;
+		return { "#" + middle + str + (parity ? " " : "") + middle + "#\n" };
 	}
 	catch (const std::exception& ex) {
 		std::cout << ex.what();
-		return std::string();
+		return {};
 	}
 }
 
 
 template <class TypeArray>
 constexpr const std::string WORK::Interface<TypeArray>
-::generatingStrings(const std::string& str, const std::string& str2, char del) const
+::generatingStrings(const std::string&& inStr, const std::string&& inStr2, char del) const
 {
-	return generatingStrings(std::move(str), std::move(str2), std::move(del));
-}
+	if (inStr.empty() || inStr2.empty()) return {};
 
+	std::string str;
+	std::string str2;
+	if constexpr (std::is_lvalue_reference_v<std::string>)
+	{
+		str = inStr;
+		str2 = inStr2;
+	}
+	else
+	{
+		str = std::move(inStr);
+		str2 = std::move(inStr2);
+	}
 
-template <class TypeArray>
-constexpr const std::string WORK::Interface<TypeArray>
-::generatingStrings(const std::string&& str, const std::string&& str2, char del) const
-{
 	try {
 		size_t len{ str.length() + str2.length() + 11 };
 		size_t middleSize{ maxTableWidth > len ? maxTableWidth - len : 11 };
-
 		std::string middle(middleSize, del);
-		std::string result{ "#    " + str + middle + str2 + "    #\n" };
 
-		return result;
+		return { "#    " + str + middle + str2 + "    #\n" };
 	}
 	catch (const std::exception& ex) {
 		std::cout << ex.what();
-		return std::string();
+		return {};
 	}
 }
--- Sem3WORK/Sem3WORK.cpp
+++ Sem3WORK/Sem3WORK.cpp
@@ -20,11 +20,10 @@ int main()
     {
         //if (window.getFlagClearArrayAndHash()) window.showGeneratedRandom();
 
-        system("cls");                      // Очистка экрана
-        //window.showHeader();                // Вывод заголовка
-        //window.showStatusType();            // Вывод служебной информации
-        //window.showStatusBar();             // Вывод "всплывающих сообщений"
-        //window.showMenu();                  // Вывод меню
-        //window.readKey();
+        system("cls");                        // Очистка экрана
+        window.showHeader();                // Вывод заголовка
+        window.showStatusBar();             // Вывод "всплывающих сообщений"
+        window.showMenu();                  // Вывод меню
+        window.readKey();
     }
 }
